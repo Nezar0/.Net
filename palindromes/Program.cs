47187075a197be485c98745a7aa797d2f58fe772
Nezar0 <62740373+Nezar0@users.noreply.github.com>
2020-06-23 15:48:33 +0700

    Add files via upload

---
+++ palindromes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c =Int32.Parse(Console.ReadLine());
+            Console.WriteLine(c.Palidrom());
+            Console.WriteLine();
+        }
+    }
+    public static class PalidromeSearch
+    {
+        public static bool Palidrom(this int number)
+        {
+            bool ok = true;
+            int temp = number; 
+            int b = 0; 
+
+            while (temp != 0)
+            {
+                b = b * 10 + temp % 10;
+                temp /= 10;
+            }
+
+            if (number == b)
+                ok = true;
+            else
+                ok = false;
+            return ok;
+        }
+    }
+}
